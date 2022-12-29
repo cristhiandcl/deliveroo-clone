0786f83110b46a1acd5a4f294bbb881d3b60a8c4
Cristhian Carrillo <carrillocristhian2408@gmail.com>
2022-12-29 13:43:39 -0500

    new feature

--- components/DishCard.js
+++ components/DishCard.js
@@ -6,8 +6,14 @@ import Currency from "react-currency-formatter";
 
 const DishCard = ({ id, description, name, image, price }) => {
   const [amount, setAmount] = useState(0);
+  const [isPressed, setIsPressed] = useState(false);
   return (
-    <View>
+    <TouchableOpacity
+      className={`border-b-[1px] border-gray-200 py-4 ${
+        isPressed && "border-b-0"
+      }`}
+      onPress={() => setIsPressed(!isPressed)}
+    >
       <View className="flex-row">
         <View className="flex-1 space-y-2">
           <Text className="text-2xl">{name}</Text>
@@ -18,18 +24,20 @@ const DishCard = ({ id, description, name, image, price }) => {
         </View>
         <Image source={{ uri: urlFor(image).url() }} className="h-20 w-20" />
       </View>
-      <View className="px-1 flex-row space-x-1 items-center pt-3 pb-5">
-        <TouchableOpacity
-          onPress={() => (amount > 0 ? setAmount(amount - 1) : setAmount(0))}
-        >
-          <MinusCircleIcon size={40} color="green" />
-        </TouchableOpacity>
-        <Text>{amount}</Text>
-        <TouchableOpacity onPress={() => setAmount(amount + 1)}>
-          <PlusCircleIcon size={40} color="green" />
-        </TouchableOpacity>
-      </View>
-    </View>
+      {isPressed && (
+        <View className="px-1 flex-row space-x-1 items-center pt-3">
+          <TouchableOpacity
+            onPress={() => (amount > 0 ? setAmount(amount - 1) : setAmount(0))}
+          >
+            <MinusCircleIcon size={40} color="green" />
+          </TouchableOpacity>
+          <Text>{amount}</Text>
+          <TouchableOpacity onPress={() => setAmount(amount + 1)}>
+            <PlusCircleIcon size={40} color="green" />
+          </TouchableOpacity>
+        </View>
+      )}
+    </TouchableOpacity>
   );
 };
 
